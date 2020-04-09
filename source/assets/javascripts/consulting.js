80097d7f5a818327aedd72a9443c09ed107b2ede
Kai Zau <k@kaizau.com>
2020-04-09 20:46:25 +0800

    Reenable image gallery

--- source/assets/javascripts/consulting.js
+++ source/assets/javascripts/consulting.js
@@ -7,8 +7,32 @@ const overlayTarget = overlay.querySelector(".cell");
 images.forEach((image) => {
   image.addEventListener("click", (e) => {
     e.preventDefault();
-    const src = e.target.href;
-    // TODO
-    console.log("add image", src);
+    let target = e.target;
+    while (target && target.nodeName !== "A") {
+      target = e.target.parentElement;
+    }
+
+    const image = document.createElement("img");
+    image.addEventListener("load", function loaded() {
+      image.removeEventListener("load", loaded);
+      overlayTarget.classList.add("loaded");
+    });
+    image.src = target.href;
+
+    overlayTarget.innerHTML = "";
+    overlayTarget.appendChild(image);
+
+    if (target.classList.contains("mobile")) {
+      overlay.classList.add("mobile");
+    } else {
+      overlay.classList.remove("mobile");
+    }
+
+    overlay.classList.add("active");
   });
+});
+
+overlay.addEventListener("click", (e) => {
+  overlay.classList.remove("active");
+  overlayTarget.classList.remove("loaded");
 });
